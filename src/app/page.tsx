f96f4b889ab6e552be31edf8754aa73f2296448a
saaqllain212 <saaqllain@protonmail.com>
2026-01-12 12:51:37 +0530

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+'use client'; // 👈 Add this line to make the button work
 import LandingPage from "@/components/landing/LandingPage";
 
 export default function Page() {
-  return <LandingPage />;
+  return (
+    <>
+      {/* 🔴 SENTRY TEST BUTTON */}
+      <button
+        onClick={() => {
+          throw new Error("Sentry Test: The Eagle has landed from UI!");
+        }}
+        className="fixed bottom-4 right-4 z-50 bg-red-600 text-white p-4 font-bold border-4 border-black shadow-neo"
+      >
+        💣 TEST ERROR
+      </button>
+
+      <LandingPage />
+    </>
+  );
 }
